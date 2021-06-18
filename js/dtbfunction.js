b5355271d05cc39ed583be1405892ae1b2a620a0
Hungng <hungnguyen35800@gmail.com>
2021-06-18 23:45:54 +0700

    new code to day, change all in folder.js

---
+++ js/dtbfunction.js
@@ -0,0 +1,5 @@
+// const users = collection("Users")
+// const passiveIncome = collection("PassiveIncome")
+// const savings = collection("Savings")
+// const activeExchanges = collection("ActiveExchanges")
+
