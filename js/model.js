c4256cc20fe7a7bf48f022d464e078e074699d03
gicxdc <75556620+gicxdc@users.noreply.github.com>
2021-06-18 12:28:23 +0700

    Create model.js

---
+++ js/model.js
@@ -0,0 +1,53 @@
+const model = {};
+model.currentUser = undefined;
+model.register = async (data) => {
+    try {
+        await firebase
+            .auth()
+            .createUserWithEmailAndPassword(data.email, data.password);
+        
+
+
+     
+        firebase.auth().currentUser.updateProfile({
+            displayName: data.lastName + " " + data.firstName
+        });
+
+
+
+        firebase.auth().currentUser.sendEmailVerification();
+
+ 
+        alert('Your email has been registered, please check your email');
+
+        view.setActiveScreen('loginScreen');
+
+    }
+    catch (err) {
+        console.log(err);
+        alert(err.message);
+    }
+}
+
+model.login = async (dataLogin) => { 
+    try {
+        const response = await firebase 
+            .auth()
+            .signInWithEmailAndPassword(dataLogin.email, dataLogin.password);
+
+    } 
+    catch (err) { 
+        console.log("err", err);
+        if (err.code == 'auth/user-not-found'  
+            || err.code == "auth/invalid-email"   
+        ) 
+        {
+            document.getElementById('email-error').innerText = err.message;
+        }
+
+        else if (err.code == 'auth/wrong-password') {
+            console.log("err:", err);
+            document.getElementById('password-error').innerText = err.message;
+        }
+    }
+}
