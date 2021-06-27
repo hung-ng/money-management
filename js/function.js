aa710f4241093c4f7bd13de6bd0e20943c167513
gicxdc <75556620+gicxdc@users.noreply.github.com>
2021-06-27 10:05:09 +0700

    Create function.js

---
+++ js/function.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px"
+  document.getElementById("theBody").style.backgroundColor = "rgba:(0, 0, 0, 0.7)"
+  document.getElementById("mySidenav").style.opacity = "1"
+  document.getElementById("test").style.opacity="0.2"
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  
+  document.getElementById("test").style.opacity="1"
+}
