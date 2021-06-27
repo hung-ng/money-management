179e808552e363dc95d091a25c4b52558853cfd9
gicxdc <75556620+gicxdc@users.noreply.github.com>
2021-06-27 10:24:55 +0700

    Update component.js

--- js/component.js
+++ js/component.js
@@ -6,6 +6,17 @@ component.homeScreen = `
 <div id="sign-out" class="cursor" > 
 Sign-out
 </div>
+<div id="mySidenav" class="sidenav">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&minus;</a>
+  <div>blance here</div>
+  <div id="quy" >quy</div>
+  <div id="thunhap">thunhap</div>
+  <div id="giaodich">giaodich</div>
+  <p class="tm"> something coporation </p>
+</div>
+  <div class="expand" onclick="openNav()"> &plus; </div>
+
+
 `;
 
 component.registerScreen = `
@@ -17,20 +28,20 @@ component.registerScreen = `
     <form id="register-form">
         <div class="input-name-wrapper">
             <div class="input-wrapper">
-                <input type="text" name="firstName" placeholder="First Name">
+                <input type="text" name="firstName" placeholder="Pls input your first name">
                 <div class="error" id="first-name-error"></div>
             </div>
             <div class="input-wrapper">
-                <input type="text" name="lastName" placeholder="Last Name">
+                <input type="text" name="lastName" placeholder="Pls input your last name">
                 <div class="error" id="last-name-error"></div>
             </div>
         </div>
         <div class="input-wrapper">
-            <input type="email" name="email" placeholder="Email">
+            <input type="email" name="email" placeholder="Input your email">
             <div class="error" id="email-error"></div>
         </div>
         <div class="input-wrapper">
-            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password" placeholder="Type your Password">
             <div class="error" id="password-error"></div>
         </div>
         <div class="input-wrapper">
@@ -39,8 +50,7 @@ component.registerScreen = `
         </div>
         <!-- xong het phan input -->
         <div class="form-action">
-            <span>Already have an account? </span>
-            <span class="cursor-pointer" id="redirect-to-login">Login</span>
+            <span class="cursor-pointer" id="redirect-to-login">Already have an account? Login</span>
         </div>
         <button class="btn" type="submit">
             Register
@@ -60,26 +70,26 @@ component.loginScreen = `
     <form id="login-form">
         
         <div class="input-wrapper">
-            <input type="email" name="email" placeholder="Email">
+            <input type="email" name="email" placeholder="Input your email">
             <div class="error" id="email-error"></div>
         </div>
         <div class="input-wrapper">
-            <input type="password" name="password" placeholder="Password">
+            <input type="password" name="password" placeholder="Type your Password">
             <div class="error" id="password-error"></div>
         </div>
         
         <!-- xong het phan input -->
         <div class="form-action">
-            <span>Don't have an account? </span>
-            <span class="cursor-pointer" id="redirect-to-register">Register</span>
+            <span class="cursor-pointer" id="redirect-to-register">Don't have an account? register</span>
         </div>
-        <button class="btn" type="submit" id="login-form">
+        <button class="btn" type="submit">
             Login
         </button>
     </form>
 </div>
 </div>
 `
+
 component.quy=`  <button class="btn" id="redierctInfo"> redirect </button>
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&minus;</a>
@@ -263,6 +273,3 @@ component.b2=` <button class="btn" id="redierctInfo"> redirect </button>
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
   </form>`
-
-
-
