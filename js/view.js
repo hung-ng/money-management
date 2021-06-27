ceefffe350779d71a1a752edb1d88478d51a765b
gicxdc <75556620+gicxdc@users.noreply.github.com>
2021-06-27 10:04:04 +0700

    Update view.js

--- js/view.js
+++ js/view.js
@@ -31,7 +31,7 @@ view.setActiveScreen = (screenName) => {
 
             const loginForm = document.getElementById('login-form');
             loginForm.addEventListener('submit', (event) => {
-                event.preventDefault(); 
+                event.preventDefault();
                 const dataLogin = {
                     email: loginForm.email.value,
                     password: loginForm.password.value
@@ -44,12 +44,115 @@ view.setActiveScreen = (screenName) => {
         case "homeScreen":
             document.getElementById('main').innerHTML = component.homeScreen;
             const signOutButton = document.getElementById("sign-out");
-            signOutButton.addEventListener('click', ()=>{
+            signOutButton.addEventListener('click', () => {
                 firebase.auth().signOut();
                 view.setActiveScreen("loginScreen");
+            }
+            );
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
             })
-        break;
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            break;
+
+        case "quy":
+            document.getElementById('main').innerHTML = component.quy;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('redirectInfo').addEventListener('click', () => {
+                view.setActiveScreen('b1');
+           
+            })
+            break;
+        case "thunhap":
+            document.getElementById('main').innerHTML = component.thunhap;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            document.getElementById('redirectInfo').addEventListener('click', () => {
+                view.setActiveScreen('b1');})
+            break;
+        case "giaodich":
+            document.getElementById('main').innerHTML = component.giaodich;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            document.getElementById('c1').addEventListener('click', () => {
+                view.setActiveScreen('c1');
+            })
+            document.getElementById('c2').addEventListener('click', () => {
+                view.setActiveScreen('c2');
+            })
+
+            break;
+        case "b1":
+            document.getElementById('main').innerHTML = component.b1;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            break;
+        case "b2":
+            document.getElementById('main').innerHTML = component.b2;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            break;
+        case "c1":
+            document.getElementById('main').innerHTML = component.c1;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            break;
+        case "c2":
+            document.getElementById('main').innerHTML = component.c2;
+            document.getElementById('quy').addEventListener('click', () => {
+                view.setActiveScreen('quy');
+            })
+            document.getElementById('thunhap').addEventListener('click', () => {
+                view.setActiveScreen('thunhap');
+            })
+            document.getElementById('giaodich').addEventListener('click', () => {
+                view.setActiveScreen('giaodich');
+            })
+            break;
     }
 }
-
-
